fa6d8ef3bdd0b221fe7f3739e5e45343d35df8cb
Alex Mercer <kalagaev.alex.a@gmail.com>
2022-09-17 21:58:41 +0300

    Js update 17.09

--- Project/JS/Menu.js
+++ Project/JS/Menu.js
@@ -1,27 +1,45 @@
-topButton = () => {
-  const topButton = document.querySelector('.menu');
-  topButton.scrollIntoView(
-    {block: "center", inline: "nearest", behavior: "smooth"}
-    );
+// start Top button 
+
+window.onscroll = () => {scrollCoord()};
+
+scrollCoord = () => {
+    if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+        document.getElementById("topBtn").style.display = "block";
+    } else {
+        document.getElementById("topBtn").style.display = "none";
+    }
 }
+
+topButton = () => {
+  const topButton = document.querySelector('header');
+  topButton.scrollIntoView({
+        block: "start", inline: "nearest", behavior: "smooth"
+    });
+}
+
+//end Top button
+
+// start Menu button
 
 stuffAndComp = () => {
 	const stuffAndComp = document.querySelector('.stuff-and-comp');
-	stuffAndComp.scrollIntoView(
-    {block: "center", inline: "nearest", behavior: "smooth"}
-    );
+	stuffAndComp.scrollIntoView({
+        block: "center", inline: "nearest", behavior: "smooth"
+    });
 };
 
 serviceAndPrice = () => {
 	const serviceAndPrice = document.querySelector('.service-and-price');
-	serviceAndPrice.scrollIntoView(
-    {block: "start", inline: "nearest", behavior: "smooth"}
-    );
+	serviceAndPrice.scrollIntoView({
+        block: "center", inline: "nearest", behavior: "smooth"
+    });
 };
 
 contactDetails = () => {
 	const contactDetails = document.querySelector('.contact-details');
-	contactDetails.scrollIntoView(
-    {block: "start", inline: "nearest", behavior: "smooth"}
-    );
+	contactDetails.scrollIntoView({
+        block: "start", inline: "nearest", behavior: "smooth"
+    });
 };
+
+// end Menu button
