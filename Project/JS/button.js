a56a0c2071b93bcb789a34c5e60c010b3124f7ee
Alex Mercer <kalagaev.alex.a@gmail.com>
2022-09-21 00:58:25 +0300

    Dirty JS update 21.09

--- Project/JS/button.js
+++ Project/JS/button.js
@@ -20,29 +20,60 @@ topButton = () => {
 //end Top button
 
 //start Burger button
-const openButton = document.getElementById("opnBrg");
-const closeButton = document.getElementById("clsBrg");
-const openeerDiv = document.querySelector('.opner');
 
-openBurger = () => {
-    openButton.style.display = "none";
-    closeButton.style.display = "block";
-    openeerDiv.style.cssText = 
-        `
-        display: block;    
-        height: 100%; 
-        width: 100%; 
-        position: fixed; 
-        backdrop-filter: blur(10px);
+/*if (document.documentElement.clientWidth < 1100) {*/
+
+    const openButton = document.getElementById("opnBrg");
+    const closeButton = document.getElementById("clsBrg");
+    const openerDiv = document.querySelector('.opner');
+    const bugerNav = document.querySelector('.burger');
+    
+    openBurger = () => {
+        openButton.style.display = "none";
+        closeButton.style.display = "block";
+        bugerNav.style.display = "grid";
+        openerDiv.style.cssText = `
+                                    display: block;    
+                                    height: 100%; 
+                                    width: 100%; 
+                                    position: fixed; 
+                                    backdrop-filter: blur(10px);
         `;
-    document.body.style.overflow = "hidden"; 
-}
-closeBurger = () => {
-    openButton.style.display = "block";
-    closeButton.style.display = "none";
-    openeerDiv.style.cssText = `display: none;`;
-    document.body.style.overflow = ""; 
-}
+        document.body.style.overflow = "hidden"; 
+    }
+
+    closeBurger = () => {
+        openButton.style.display = "block";
+        closeButton.style.display = "none";
+        openerDiv.style.display = "none"; //cssText = `display: none;`;
+        bugerNav.style.display = "none";
+        
+        document.body.style.overflow = ""; 
+    }
+
+    /*stuffAndComp= () => {
+        closeBurger();
+        
+    } 
+    serviceAndPrice= () => {
+        openButton.style.display = "block";
+        closeButton.style.display = "none";
+        openerDiv.style.display = "none"; 
+        bugerNav.style.display = "none";
+        
+    }
+    contactDetails= () => {
+        openButton.style.display = "block";
+        closeButton.style.display = "none";
+        openerDiv.style.display = "none"; 
+        bugerNav.style.display = "none";
+        
+    }*/
+/*} else if (document.documentElement.clientWidth > 1100) {    
+
+    document.getElementById("opnBrg").style.display = "none";
+    document.getElementById("clsBrg").style.display = "none";
+}*/
 
 //end Burger button
 
